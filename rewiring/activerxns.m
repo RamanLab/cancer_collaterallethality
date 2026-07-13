a5381714c058c524e16e60c967f4474adb463c42
maziya <maziya.ibrahim@gmail.com>
2026-07-13 17:23:12 +0530

    Update activerxns.m

--- rewiring/activerxns.m
+++ rewiring/activerxns.m
@@ -10,7 +10,7 @@ for i = 1:length(csvFiles)
     filePath = fullfile(inputFolderPath, csvFiles(i).name);
     data = readtable(filePath);
     data = rmmissing(data);
-    filteredData = data((data.FluxCancer == 0 & data.FluxNormal ~= 0) | (data.FluxNormal == 0 & data.FluxCancer ~=0), :);
+    filteredData = data((data.FluxCancer == 0 & data.FluxNormal >abs(0.01) | (data.FluxNormal == 0 & data.FluxCancer >abs(0.01), :);
     [~, fileName, ext] = fileparts(csvFiles(i).name);
     outputFileName = fullfile(outputFolderPath, [fileName, '_filtered', ext]);
     writetable(filteredData, outputFileName);
